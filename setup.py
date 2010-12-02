e41effbb61d848aa58a701db304f10d4d8c45242
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2010-12-02 13:11:39 -0500

    various changes

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-  	name = 'pyzalgo',
-	version = '0.1',
-	description = "turn your text into ZALGO text",
-	author = "Mike Giarlo",
-	author_email = "michael.giarlo@gmail.com",
-	url = "http://github.com/mjgiarlo/pyzalgo",
-	py_modules = ['zalgo', 'ez_setup'],
-	test_suite = '',
-	scripts = ['bin/zalgo']
+    name = 'pyzalgo',
+    version = '0.2.1',
+    description = "turn your text into ZALGO text",
+    author = "Mike Giarlo",
+    author_email = "michael.giarlo@gmail.com",
+    url = "http://github.com/mjgiarlo/pyzalgo",
+    py_modules = ['zalgo', 'ez_setup'],
+    requires = ["argparse"],
+    scripts = ['bin/zalgo']
 )
--- zalgo.py
+++ zalgo.py
@@ -1,15 +1,11 @@
 # coding: utf-8
 from random import randint, choice
 
-def zalgo(text):
-    """[--light] text
-    ZALGO"""
-    zalgo_threshold = 50
+def zalgo(text, intensity=50):
+    zalgo_threshold = intensity
     zalgo_chars = [unichr(i) for i in range(0x0300, 0x036F + 1)]
     zalgo_chars.extend([u'\u0488', u'\u0489'])
-    random_extras = [unichr(i) for i in range(0x0FBE, 0x0FD4 + 1)]
-    random_extras.extend([unichr(i) for i in range(0x0F00, 0x0F17 + 1)])
-    random_extras.extend([unichr(i) for i in range(0x1D023, 0x1D045 + 1)])
+    random_extras = [unichr(i) for i in range(0x1D023, 0x1D045 + 1)]
     def insert_randoms(text):
         newtext = []
         for char in text:
@@ -26,3 +22,4 @@ def zalgo(text):
             zalgoized.append(choice(zalgo_chars))
     response = choice(zalgo_chars).join(zalgoized)
     return response.encode('utf8', 'ignore')
+
